3d19c2b4bd5309763275fb8aad147cbee1a50689
Faheim Arslan M <faheimarslan@gmail.com>
2022-03-30 13:57:02 +0100

    added nav bar skeletal structure

---
+++ portfolio/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
