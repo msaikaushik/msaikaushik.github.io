b1460e396c04226e5db24217699e15f33177a27b
Faheim Arslan M <faheimarslan@gmail.com>
2022-03-31 13:39:43 +0100

    added basic content in each page

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <div>Hi! I'm Sai Kaushik.</div>
+        <div>I'm a Software development Engineer.</div>
+        <div>I'm a Digital Artist and 3D Modeller.</div>
+        <div>I love hiking, basketball, Piano, Art, and Swimming.</div>
+        <div>Also love dogs. They're cute!</div>
+    </div>
+  )
+}
---
+++ portfolio/src/components/Skills.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+//Idea TODO: a clickable VR machine (the one princess leia uses to communicate with 
+//obi wan and luke) with a graph of all these main clickable skills.
+//when clicked, it should reveal all the libraries and frameworks under that language.
+//a box like database beside it, which shows all the DB techs you know.
+export default function Skills() {
+  return (
+    <>
+      <div>Skills</div>
+      <ul>
+        <li>C++</li>
+        <li>Java</li>
+        <li>Python</li>
+        <li>Javascript</li>
+        <li>ReactJS</li>
+        <li>Spring Boot</li>
+        <li>SQL</li>
+        <li>MongoDB</li>
+      </ul>
+    </>
+  );
+}
