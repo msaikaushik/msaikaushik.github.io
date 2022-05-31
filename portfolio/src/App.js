5336e0bc909dd92a23e5d59bca614207e76e3f2f
Faheim Arslan M <faheimarslan@gmail.com>
2022-05-31 17:25:48 +0100

    added main background image

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import BackgroundPage from './components/BackgroundPage';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <BackgroundPage></BackgroundPage>
     </div>
   );
 }
---
+++ portfolio/src/components/BackgroundPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './styles/BackgroundPage.css';
+
+export default function BackgroundPage() {
+  return (
+    <section>
+        {/* <img src="images/background.png" alt="sky"/> */}
+        <h2>Kaushik</h2>
+        {/* TODO: Look into Fonts. */}
+        <img src="images/level6.png" alt="faraway_mountains"/>
+        <img src="images/level5.png" alt="mountains"/>
+        <img src="images/level4.png" alt="river"/>
+        <img src="images/level3.png" alt="lake"/>
+        <img src="images/level2.png" alt="treeline"/>
+        <img src="images/level1.png" alt="foreground"/>
+    </section>
+  );
+}
