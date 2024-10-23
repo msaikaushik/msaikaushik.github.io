358dba99e7aa22fdc2a07e1acf5aa15c55c54ca3
Sai Kaushik <m.saikaushik97@gmail.com>
2024-10-23 17:01:21 +0100

    added few projects and skills

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,17 +1,21 @@
 const projectsData = [
     {
-      title: 'Project One',
-      description: 'This is a description for project one.',
+      title: 'Time Series Analysis of Dublin Bikes',
+      description: `Conducted a comprehensive time series analysis to forecast the availability of Dublin bike stations.
+                    Developed machine learning models to predict short-term bike availability, using historical data on bike rentals and returns.
+                    The models aimed to improve user experience by helping riders plan ahead, ensuring bikes or docking stations are available when needed.`,
       image: 'https://via.placeholder.com/400x200',
-      technologies: ['React', 'Tailwind CSS'],
+      technologies: ['Python', 'Scikit Learn'],
       liveLink: 'https://live-demo.com',
       githubLink: 'https://github.com/yourusername/project-one',
     },
     {
-      title: 'Project Two',
-      description: 'This is a description for project two.',
+      title: 'UML to Java persistent object layer translator',
+      description: `Developed a software which can take UML data models ofthe architecture and convert them into spring
+                    boot code. Supported creation of Repositories, Interfaces, Entities, Cardinality relationships between
+                    entities, Enum datatypes, Controllers, among others.`,
       image: 'https://via.placeholder.com/400x200',
-      technologies: ['JavaScript', 'HTML', 'CSS'],
+      technologies: ['Java', 'UML'],
       liveLink: 'https://live-demo.com',
       githubLink: 'https://github.com/yourusername/project-two',
     },
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,8 +1,11 @@
 const skillsData = [
   { id: 1, name: 'JavaScript', level: 'Advanced', description: 'Proficient in ES6+' },
   { id: 2, name: 'React', level: 'Advanced', description: 'Experienced with hooks, context, and more' },
-  { id: 3, name: 'Node.js', level: 'Intermediate', description: 'Experience building APIs' },
-  // Add more skills as needed
+  { id: 3, name: 'Java', level: 'Intermediate', description: 'Experience building APIs' },
+  { id: 4, name: 'C++', level: 'Intermediate', description: 'Experience building APIs' },
+  { id: 5, name: 'Python', level: 'Intermediate', description: 'Experience building APIs' },
+  { id: 5, name: 'Spring', level: 'Intermediate', description: 'Experience building APIs' },
+// Add more skills as needed
 ];
 
 export default skillsData;
